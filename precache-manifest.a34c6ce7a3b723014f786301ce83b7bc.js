567ca478e0294e1d5400d21431f93085bb099239
shalini0316 <shalini.jay1603@gmail.com>
2020-07-17 14:25:22 +0530

    Updates

---
+++ precache-manifest.a34c6ce7a3b723014f786301ce83b7bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4520ebde2c4693eb2979b00096899b",
+    "url": "/React-todo/index.html"
+  },
+  {
+    "revision": "c0bb3ef17fec37485d49",
+    "url": "/React-todo/static/css/main.031e540d.chunk.css"
+  },
+  {
+    "revision": "f8538519eedb7a64242e",
+    "url": "/React-todo/static/js/2.ad5dea97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todo/static/js/2.ad5dea97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0bb3ef17fec37485d49",
+    "url": "/React-todo/static/js/main.fc2ec45c.chunk.js"
+  },
+  {
+    "revision": "a6bdfb413bfde76e5dbe",
+    "url": "/React-todo/static/js/runtime-main.497ab224.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todo/static/media/logo.5d5d9eef.svg"
+  }
+]);
